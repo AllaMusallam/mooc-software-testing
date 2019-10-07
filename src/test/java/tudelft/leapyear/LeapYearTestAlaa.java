d3403e193eaca09e318c47bbfc0b3094c2793829
AlaaMusallam <alla.j.musallam@gmail.com>
2019-10-08 01:39:23 +0300

    LeapYearTestAlaa.java

--- src/test/java/tudelft/leapyear/LeapYearTestAlaa.java
+++ src/test/java/tudelft/leapyear/LeapYearTestAlaa.java
@@ -1,4 +1,38 @@
 package tudelft.leapyear;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class LeapYearTestAlaa {
+    private LeapYearAlaa leap;
+
+    @BeforeEach
+    public void initialization(){
+        this.leap = new LeapYearAlaa();
+    }
+
+    @Test
+    public void leapYear(){
+        boolean isLeap = this.leap.isLeapYear(2020);
+        Assertions.assertTrue(isLeap);
+    }
+
+    @Test
+    public void nonLeapYear(){
+        boolean isLeap = this.leap.isLeapYear(2019);
+        Assertions.assertFalse(isLeap);
+    }
+
+    @Test
+    public void leapCenturialYear(){
+        boolean isLeap = this.leap.isLeapYear(2000);
+        Assertions.assertTrue(isLeap);
+    }
+
+    @Test
+    public void nonLeapCenturialYear(){
+        boolean isLeap = this.leap.isLeapYear(1700);
+        Assertions.assertFalse(isLeap);
+    }
 }
